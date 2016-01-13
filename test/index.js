acb706936c178b0bfdabeb49505dd334f39ad0b7
Nick <nick@jam3.com>
2016-01-13 17:27:42 -0500

    removed extra console log

--- test/index.js
+++ test/index.js
@@ -9,8 +9,3 @@ var test = createWhere({
 });
 var result = "`a` = 'a' AND `b` >= 'b' AND `c` <= 'c' AND `d` < 'd' AND `e` > 'e' AND `f` != 'f'";
 console.log(test===result ? '\x1b[32m passed!' : '\x1b[31m failed!');
-console.log(createWhere({
-  category: 'blog',
-  date: {$gte: '2016-01-12', $lte: '2016-01-14'},
-  author: {$ne: 'steve'}
-}))
