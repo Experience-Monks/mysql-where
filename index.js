6e1f3efd5e43d0560e4725582c5838d0f46e9a68
Nick <nick@jam3.com>
2016-06-07 23:43:49 -0400

    Fixed numeric vals

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ module.exports = function(where,join) {
   join = join || ' AND ';
   var query = [];
   var val;
-  for (var key in where) {
+  Object.keys(where).forEach(function(key) {
     val = where[key];
-    if (typeof val ==='string') {
-      query.push(db.escapeId(key)+' = '+db.escape(val));
-    } else {
+    if (val !== null && typeof val === 'object') {
       if (val.$lte!==undefined) query.push(db.escapeId(key)+' <= '+db.escape(val.$lte));
       if (val.$gte!==undefined) query.push(db.escapeId(key)+' >= '+db.escape(val.$gte));
       if (val.$lt!==undefined) query.push(db.escapeId(key)+' < '+db.escape(val.$lt));
       if (val.$gt!==undefined) query.push(db.escapeId(key)+' > '+db.escape(val.$gt));
       if (val.$ne!==undefined) query.push(db.escapeId(key)+' != '+db.escape(val.$ne));
+    } else {
+      query.push(db.escapeId(key)+' = '+db.escape(val));
     }
-  }
+  });
   return query.join(join);
 };
